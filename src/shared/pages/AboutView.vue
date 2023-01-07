7ba0ddf99906e6fc2add61945a547e47e7df1e6a
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-01-06 22:17:03 -0500

    first structure

---
+++ src/shared/pages/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
