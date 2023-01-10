5deee1086942cff3a03742376a4c8ca21daa57a2
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-01-09 23:37:52 -0500

    adding use query

--- src/modules/characters/components/CardList.vue
+++ src/modules/characters/components/CardList.vue
@@ -1,18 +1,44 @@
 <template>
   <h3 v-if="isLoading">Loading...</h3>
-  <h3 v-if="hasError">{{ errorMessage }}</h3>
-  <ul>
-    <li v-for="{ id, name } of characters" :key="id">
-      {{ name }}
-    </li>
+  <ul class="card-list">
+    <CharacterCard
+      v-for="character of characters"
+      :key="character.id"
+      :character="character"
+    />
   </ul>
 </template>
 
 <script setup lang="ts">
-import { useCharacters } from '@/modules/characters/composables/useCharacters'
+import { useQuery } from '@tanstack/vue-query'
+import CharacterCard from '@/modules/characters/components/CharacterCard.vue'
+import rickyAndMorphyApi from '@/api/rickyAndMorphyApi'
+import type { Character, RickyAndMortyResponse } from '../interfaces/character'
 
-const { characters, isLoading, hasError, errorMessage } = useCharacters()
+const getCharactersSlow = (): Promise<Character[]> => {
+  return new Promise((resolve) => {
+    setTimeout(async () => {
+      const { data } = await rickyAndMorphyApi.get<RickyAndMortyResponse>(
+        '/character'
+      )
+      resolve(data.results)
+    }, 1)
+  })
+}
 
+const { isLoading, data: characters } = useQuery({
+  queryKey: ['characters'],
+  queryFn: getCharactersSlow,
+  cacheTime: 1000 * 2,
+  refetchOnReconnect: 'always',
+  refetchOnWindowFocus: true,
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.card-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/modules/characters/components/CharacterCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="character-card">
+    <img loading="lazy" :src="character.image" @error="setImage" :alt="character.name" />
+    <h3>{{ character.name }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRef } from 'vue'
+import avatar from '@/assets/avatar.png'
+import type { Character } from '../interfaces/character'
+
+interface Props {
+  character: Character
+}
+
+const props = defineProps<Props>()
+const character = toRef(props, 'character')
+
+const setImage = (event: Event) => {
+  const target = event.target as HTMLImageElement
+  target.src = avatar
+}
+</script>
+
+<style scoped>
+.character-card {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  margin-left: 5px;
+  margin-right: 5px;
+  overflow: hidden;
+  width: 150px;
+}
+
+img {
+  border-radius: 5px 5px 0 0;
+  box-shadow: 0 2px 10px rgba(255, 255, 255, 0.1);
+  max-width: 100%;
+  height: auto;
+}
+</style>
